0ef554d3a832a1fb05e3fec45ffefc34d914e413
Fmiri08 <71121534+Fmiri08@users.noreply.github.com>
2021-11-26 11:08:13 +0100

    axios hozzáadva és első lekérdezés megírva

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,24 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const Home = () => {
-  return <div>asd</div>;
+  const [games, setGames] = useState([]);
+  const [doDownload, setDoDownload] = useState(false);
+  useEffect(() => {
+    const download = async () => {
+      try {
+        const { data } = await axios.get(
+          "https://api.boardgameatlas.com/api/search?name=Catan&client_id=nPjnxTDLZk"
+        );
+        setGames(data);
+        setDoDownload(true);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    download();
+  }, [doDownload]);
+  return <div>{doDownload && <div>{games.games[0].id}</div>}</div>;
 };
 
 export default Home;
