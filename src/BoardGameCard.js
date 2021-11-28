9d7c5117ef2493c64d0b50c4405e2bbad2c5918f
Fmiri08 <71121534+Fmiri08@users.noreply.github.com>
2021-11-28 18:17:39 +0100

    Társasjátékok keresése működik

--- src/BoardGameCard.js
+++ src/BoardGameCard.js
@@ -1,9 +1,14 @@
 import { Link } from "react-router-dom";
 
-const BoardGameCard = ({ id }) => {
+const BoardGameCard = ({ id, name, image_url }) => {
   return (
     <Link to={`/id/${id}`}>
-      <div className="gamecard">{id}</div>
+      <div className="gamecard">
+        <div>
+          <img src={image_url} alt="" className="image" />
+        </div>
+        {name}
+      </div>
     </Link>
   );
 };
--- src/Home.js
+++ src/Home.js
@@ -3,17 +3,54 @@ import axios from "axios";
 import BoardGameCard from "./BoardGameCard";
 
 const Home = () => {
+  const [gameName, setGameName] = useState("");
   const [games, setGames] = useState([]);
   const [doDownload, setDoDownload] = useState(false);
+
+  async function downloadData() {
+    try {
+      console.log(gameName);
+      const { data } = await axios.get(
+        `https://api.boardgameatlas.com/api/search?name=${gameName}
+         &fields=id,name,image_url&fuzzy_match=true&client_id=nPjnxTDLZk`
+      );
+
+      setGames(
+        data.games.map((game) => (
+          <BoardGameCard
+            key={game.id}
+            id={game.id}
+            name={game.name}
+            image_url={game.image_url}
+          />
+        ))
+      );
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  function setName(event) {
+    setGameName(event.target.value);
+  }
+
   useEffect(() => {
     const download = async () => {
       try {
         const { data } = await axios.get(
-          "https://api.boardgameatlas.com/api/search?name=Catan&client_id=nPjnxTDLZk"
+          `https://api.boardgameatlas.com/api/search?name=${gameName}
+           &fields=id,name,image_url&client_id=nPjnxTDLZk`
         );
 
         setGames(
-          data.games.map((game) => <BoardGameCard key={game.id} id={game.id} />)
+          data.games.map((game) => (
+            <BoardGameCard
+              key={game.id}
+              id={game.id}
+              name={game.name}
+              image_url={game.image_url}
+            />
+          ))
         );
         setDoDownload(true);
       } catch (e) {
@@ -23,7 +60,15 @@ const Home = () => {
     download();
   }, [doDownload]);
 
-  return <div className="cardDiv">{doDownload && games}</div>;
+  return (
+    <div>
+      <div>
+        <input value={gameName.value} onChange={setName}></input>
+        <button onClick={downloadData}>Keresés</button>
+      </div>
+      <div className="cardDiv">{doDownload && games}</div>
+    </div>
+  );
 };
 
 export default Home;
