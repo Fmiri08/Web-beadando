be1aea95c95455370d3defb7e7efd10b07235be3
Fmiri08 <71121534+Fmiri08@users.noreply.github.com>
2021-12-05 17:10:51 +0100

    useReducer kijavítva, és useContext hozzáadva

--- src/Game.js
+++ src/Game.js
@@ -1,4 +1,4 @@
-import { useEffect, useReducer, useState } from "react";
+import React, { useContext, useEffect, useReducer, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import parse from "html-react-parser";
@@ -8,19 +8,26 @@ const Game = () => {
   const [game, setGame] = useState([]);
   const [doDownload, setDoDownload] = useState(false);
   const [favourite, setFavourite] = useState(false);
-  const [state, dispatch] = useReducer(reducer, {
+  const [list, dispatch] = useReducer(reducer, {
     list: localStorage.getItem("favourites")
   });
+  const buttonTextContext = React.createContext(modifyButtonText());
+  const buttonText = useContext(buttonTextContext);
+
+  function modifyButtonText() {
+    if (!favourite) {
+      return "Add to favourites";
+    } else {
+      return "Delete from favourites";
+    }
+  }
 
   function reducer(state, action) {
-    console.log(state);
     switch (action) {
       case "add":
-        addFavourite(state.list);
-        break;
+        return addFavourite(state.list);
       case "remove":
-        removeFavourite(state.list);
-        break;
+        return removeFavourite(state.list);
       default:
         throw new Error();
     }
@@ -37,11 +44,13 @@ const Game = () => {
     state = state + id + ",";
     localStorage.setItem("favourites", state);
     isFavourite();
+    return { list: state };
   }
   function removeFavourite(state) {
     state = state.replace("" + id + ",", "");
     localStorage.setItem("favourites", state);
     isFavourite();
+    return { list: state };
   }
 
   function click() {
@@ -68,9 +77,7 @@ const Game = () => {
       <img className="image" alt="" src={game.image_url}></img>
       <div>{doDownload && game.name}</div>
       <div>{doDownload && parse(game.description.toString())}</div>
-      <button onClick={click}>
-        {favourite ? "Delete from favourites" : "Add to favourites"}
-      </button>
+      <button onClick={click}>{buttonText}</button>
     </div>
   );
 };
